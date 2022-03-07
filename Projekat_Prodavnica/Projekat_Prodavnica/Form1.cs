412e98c7a8acf69696f62a1fb91494efb8f864fd
MedzidJasarovic <96300271+MedzidJasarovic@users.noreply.github.com>
2022-03-07 15:29:28 +0100

    Add files via upload

---
+++ Projekat_Prodavnica/Projekat_Prodavnica/Form1.cs
@@ -0,0 +1,34 @@
+namespace Projekat_Prodavnica
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void radniciToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frm_Radnici Radnici = new frm_Radnici();
+            Radnici.Show();
+        }
+
+        private void poslovniPArtneriToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frm_PP PP = new frm_PP();
+            PP.Show();
+        }
+
+        private void artikliToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frm_Artikli Artikli = new frm_Artikli();
+            Artikli.Show();
+        }
+
+        private void sToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Ulaz_Kalkulacija Uk = new Ulaz_Kalkulacija();
+            Uk.Show();
+        }
+    }
+}
